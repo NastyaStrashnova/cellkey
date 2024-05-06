1599479302ac3caac782a08f98da8a0f39e4ed20
NastyaStrashnova <60945623+NastyaStrashnova@users.noreply.github.com>
2024-05-06 13:53:05 +0200

    Update cellkey.py

--- cellkey.py
+++ cellkey.py
@@ -137,12 +137,3 @@ class CellKey:
         freq_table.drop('ckeys', axis=1, inplace=True)
         return freq_table
     
-
-df = pd.read_csv("covid_munich_january_2023.csv")
-print("Original data:\n",df.head(10))
-ck = CellKey(df)
-ck.add_row_key()
-freq_table = ck.generate_frequency_table(["gender", "age_group"])
-ck.generate_perturbation_table()
-result = ck.apply_method(freq_table)
-print("with cellkey:\n",result)
